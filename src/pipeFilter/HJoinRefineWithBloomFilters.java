3ee71e5678b904137207677fef725744f34a2468
Sean Boose <seanboose@gmail.com>
2016-05-03 15:27:37 -0500

    Implemented refined hjoin as it's own class (didn't realize I was supposed to do that earlier)

---
+++ src/pipeFilter/HJoinRefineWithBloomFilters.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pipeFilter;
+
+import basicConnector.*;
+import gammaJoin.*;
+import gammaSupport.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author terriBoose
+ */
+public class HJoinRefineWithBloomFilters extends Thread {
+    
+    private ReadEnd in1;
+    private ReadEnd in2;
+    private WriteEnd write1;
+    private WriteEnd write2;
+//    private WriteEnd out;
+//    private int key1;
+//    private int key2;
+//    private Relation r_joined;
+
+    public HJoinRefineWithBloomFilters(Connector c1, Connector c2, int jk1, int jk2, Connector o) throws Exception{
+        
+        in1 = c1.getReadEnd();
+        in2 = c2.getReadEnd();
+        
+        Connector a_bloom = new Connector("a_bloom");
+        a_bloom.setRelation(c1.getRelation());
+        Connector bloom_hjoin = new Connector("bloom_hjoin");
+        Connector bloom_filter = new Connector("bloom_filter");
+        Connector b_filter = new Connector("b_filter");
+        b_filter.setRelation(c2.getRelation());
+        Connector filter_hjoin = new Connector("filter_hjoin");
+        
+        Bloom bloom = new Bloom(a_bloom, jk1, bloom_hjoin, bloom_filter);
+        BFilter bfilter = new BFilter(bloom_filter, b_filter, jk2, filter_hjoin);
+        HJoin hjoin = new HJoin(bloom_hjoin, filter_hjoin, jk1, jk2, o);
+        
+        write1 = a_bloom.getWriteEnd();
+        write2 = b_filter.getWriteEnd();
+
+        ThreadList.add(this);
+    }
+    
+    public void run () {
+        try {
+            Tuple t1 = in1.getNextTuple();
+            while(t1 != null) {
+                write1.putNextTuple(t1);
+                t1 = in1.getNextTuple();
+            }
+            write1.close();
+            
+            Tuple t2 = in2.getNextTuple();
+            while(t2 != null) {
+                write2.putNextTuple(t2);
+                t2 = in2.getNextTuple();
+            }
+            write2.close();
+            
+        } catch (Exception ex) {
+            Logger.getLogger(HJoinRefineWithBloomFilters.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
--- src/tests/HJoinRefineTest.java
+++ src/tests/HJoinRefineTest.java
@@ -8,14 +8,11 @@ package tests;
 
 import RegTest.Utility;
 import basicConnector.Connector;
-import gammaJoin.BFilter;
-import gammaJoin.Bloom;
-import gammaJoin.HJoin;
 import gammaJoin.Print;
 import gammaJoin.ReadRelation;
-import gammaJoin.Sink;
 import gammaSupport.ThreadList;
 import org.junit.Test;
+import pipeFilter.HJoinRefineWithBloomFilters;
 
 /**
  *
@@ -27,6 +24,7 @@ public class HJoinRefineTest {
     private String outDir = "src/testOutput/";
     private String inDir = "src/tables/";
     
+
     public void join(String r1name, String r2name, int jk1, int jk2) throws Exception {
         System.out.println( "Joining " + r1name + " with " + r2name );  
         
@@ -35,28 +33,18 @@ public class HJoinRefineTest {
         ReadRelation r1 = new ReadRelation(r1name, c1); 
         Connector c2 = new Connector("input2");
         ReadRelation r2 = new ReadRelation(r2name, c2);
-        
-        Connector a_out = new Connector("a_out");
-        Connector m_out = new Connector("m_out");
-        Connector b_out = new Connector("b_out");
-        Connector joined_out = new Connector("joined_out");
-        
-        Bloom bloom = new Bloom(c1, jk1, a_out, m_out);
-        BFilter bfilter = new BFilter(m_out, c2, jk2, b_out);
-        
-        HJoin hjoin = new HJoin(a_out, b_out, jk1, jk2, joined_out);
-        
-        Print p = new Print(joined_out);
-        
+        Connector o = new Connector("output");
+        HJoinRefineWithBloomFilters hj = new HJoinRefineWithBloomFilters(c1, c2, jk1, jk2, o);
+        Print p = new Print(o);
         ThreadList.run(p);
     }
     
     @Test
-    public void HJoinRefinedTest() throws Exception{
-        Utility.redirectStdOut(outDir+"hJoinRefinedOut.txt");
+    public void HJoinTest () throws Exception{
+        Utility.redirectStdOut(outDir+"hJoinRefineTestOut.txt");
         join(inDir+"parts.txt", inDir+"odetails.txt", 0, 1);
         join(inDir+"client.txt", inDir+"viewing.txt", 0, 0);
         join(inDir+"orders.txt", inDir+"odetails.txt", 0, 0);
-        Utility.validate(outDir+"hJoinRefinedOut.txt", correctDir+"hJoinOut.txt",false);
+        Utility.validate(outDir+"hJoinRefineTestOut.txt", correctDir+"hJoinOut.txt",false);
     }
 }

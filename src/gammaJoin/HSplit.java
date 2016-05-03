b82bde359fd781873b4e3ab8c0c819c3a7d7267a
Sean Boose <seanboose@gmail.com>
2016-05-03 15:55:47 -0500

    Implemented HSplit and tests

---
+++ src/gammaJoin/HSplit.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gammaJoin;
+
+import basicConnector.*;
+import gammaSupport.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author terriBoose
+ */
+public class HSplit extends Thread{
+    
+    private ReadEnd in;
+    private WriteEnd[] outs;
+    
+    public HSplit (Connector c_in, Connector[] c_outs) {
+        
+        in = c_in.getReadEnd();
+        
+        outs = new WriteEnd[c_outs.length];
+        for(int i = 0; i< c_outs.length; ++i){
+            outs[i] = c_outs[i].getWriteEnd();
+            c_outs[i].setRelation(c_in.getRelation());
+        }
+        
+        ThreadList.add(this);
+    }
+    
+    public void run () {
+        try {
+            Tuple t = in.getNextTuple();
+            while(t != null){
+                int i = BMap.myhash(t.get(0));
+                outs[i].putNextTuple(t);
+                t = in.getNextTuple();
+            }
+            
+            for(int i=0; i<outs.length; ++i){
+                outs[i].close();
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(HSplit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
--- src/tests/HSplitTest.java
+++ src/tests/HSplitTest.java
@@ -5,6 +5,10 @@
  */
 package tests;
 
+import RegTest.Utility;
+import basicConnector.*;
+import gammaJoin.*;
+import gammaSupport.*;
 import org.junit.Test;
 
 /**
@@ -13,8 +17,107 @@ import org.junit.Test;
  */
 public class HSplitTest {
 
+    private String correctDir = "src/correctOutput/";
+    private String outDir = "src/testOutput/";
+    private String inDir = "src/tables/";
+
     @Test
-    public void HSplitTest () {
+    public void HSplitTest0 () throws Exception {
+        Utility.redirectStdOut(outDir+"hSplit0Out.txt");
+        ThreadList.init();
+
+        Connector in = new Connector("input");
+        ReadRelation r1 = new ReadRelation(inDir+"client.txt", in); 
+        Connector[] outs = new Connector[4];
+        outs[0] = new Connector("out0");
+        outs[1] = new Connector("out1");
+        outs[2] = new Connector("out2");
+        outs[3] = new Connector("out3");
         
+        HSplit hsplit = new HSplit(in, outs);
+        
+        Print p0 = new Print(outs[0]);
+        Sink s1 = new Sink(outs[1]);
+        Sink s2 = new Sink(outs[2]);
+        Sink s3 = new Sink(outs[3]);
+        
+        ThreadList.run(p0);
+
+        Utility.validate(outDir+"hSplit0Out.txt", correctDir+"hSplit0Out.txt",false);
+    }
+
+    @Test
+    public void HSplitTest1 () throws Exception {
+        Utility.redirectStdOut(outDir+"hSplit1Out.txt");
+        ThreadList.init();
+
+        Connector in = new Connector("input");
+        ReadRelation r1 = new ReadRelation(inDir+"client.txt", in); 
+        Connector[] outs = new Connector[4];
+        outs[0] = new Connector("out0");
+        outs[1] = new Connector("out1");
+        outs[2] = new Connector("out2");
+        outs[3] = new Connector("out3");
+        
+        HSplit hsplit = new HSplit(in, outs);
+        
+        Sink s0 = new Sink(outs[0]);
+        Print p1 = new Print(outs[1]);
+        Sink s2 = new Sink(outs[2]);
+        Sink s3 = new Sink(outs[3]);
+        
+        ThreadList.run(p1);
+
+        Utility.validate(outDir+"hSplit1Out.txt", correctDir+"hSplit1Out.txt",false);
+    }
+
+    @Test
+    public void HSplitTest2 () throws Exception {
+        Utility.redirectStdOut(outDir+"hSplit2Out.txt");
+        ThreadList.init();
+
+        Connector in = new Connector("input");
+        ReadRelation r1 = new ReadRelation(inDir+"client.txt", in); 
+        Connector[] outs = new Connector[4];
+        outs[0] = new Connector("out0");
+        outs[1] = new Connector("out1");
+        outs[2] = new Connector("out2");
+        outs[3] = new Connector("out3");
+        
+        HSplit hsplit = new HSplit(in, outs);
+        
+        Sink s0 = new Sink(outs[0]);
+        Sink s1 = new Sink(outs[1]);
+        Print p2 = new Print(outs[2]);
+        Sink s3 = new Sink(outs[3]);
+        
+        ThreadList.run(p2);
+
+        Utility.validate(outDir+"hSplit2Out.txt", correctDir+"hSplit2Out.txt",false);
+    }
+
+    @Test
+    public void HSplitTest3 () throws Exception {
+        Utility.redirectStdOut(outDir+"hSplit3Out.txt");
+        ThreadList.init();
+
+        Connector in = new Connector("input");
+        ReadRelation r1 = new ReadRelation(inDir+"client.txt", in); 
+        Connector[] outs = new Connector[4];
+        outs[0] = new Connector("out0");
+        outs[1] = new Connector("out1");
+        outs[2] = new Connector("out2");
+        outs[3] = new Connector("out3");
+        
+        HSplit hsplit = new HSplit(in, outs);
+        
+        Sink s0 = new Sink(outs[0]);
+        Sink s1 = new Sink(outs[1]);
+        Sink s2 = new Sink(outs[2]);
+        Print p3 = new Print(outs[3]);
+        
+        ThreadList.run(p3);
+
+        Utility.validate(outDir+"hSplit3Out.txt", correctDir+"hSplit3Out.txt",false);
     }
 }
